4ca0a8d93ecc16a89b6c8aee9ddd0075ebda1bcd
Zakariae Bakkari <zakariabakkari2006@gmail.com>
2026-05-14 20:41:00 +0100

    fix: specify host as '0.0.0.0' in app.listen for external access

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(port);
+  await app.listen(port,'0.0.0.0');
   console.log('server running on : http://localhost:' + port);
 }
 void bootstrap();
